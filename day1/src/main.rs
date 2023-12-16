bd58a41a83c82a7c48ef4a7e4252ab05294f7cc3
Khaledtf Labeb <khaledtf19@gmail.com>
2023-12-16 20:54:00 +0200

    day 1 p2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,7 +1,10 @@
 use anyhow::Result;
 use regex::Regex;
+use std::borrow::Cow;
 use std::path::PathBuf;
-use std::{str, u32};
+use std::rc::Rc;
+use std::sync::{Arc, Mutex};
+use std::{str, u32, u8};
 use tokio::fs;
 use tokio::io::AsyncReadExt;
 
@@ -9,7 +12,7 @@ use tokio::io::AsyncReadExt;
 async fn main() -> Result<()> {
     let file_path = PathBuf::from("input.text");
     part1(&file_path).await?;
-    part2(&PathBuf::from("input_text_p2.text")).await?;
+    part2(&file_path).await?;
 
     Ok(())
 }
@@ -18,12 +21,31 @@ async fn part2(file_path: &PathBuf) -> Result<()> {
     let mut fp = fs::File::open(file_path).await?;
     let mut buffer = Vec::new();
     fp.read_to_end(&mut buffer).await?;
+    let possible_numbers = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+    let mut sum: u32 = 0;
 
-    let lines: Vec<_> = str::from_utf8(&buffer)?
-        .lines()
-        .map(|line| line.replace("two", "2"))
-        .collect();
-    println!("{lines:?}");
+    let lines = str::from_utf8(&buffer)?.lines();
+    for line in lines.clone() {
+        let mut nums: Vec<String> = vec![];
+        for (x, chr) in line.chars().enumerate() {
+            if chr.is_numeric() {
+                nums.push(chr.to_string());
+            }
+            let (_s, l) = line.split_at(x);
+            for (i, num) in possible_numbers.iter().enumerate() {
+                if l.starts_with(num) {
+                    nums.push((i + 1).to_string())
+                }
+            }
+        }
+        let str = nums.get(0).unwrap_or(&"0".to_string()).clone()
+            + nums.last().unwrap_or(&"0".to_string()).as_str();
+        sum += str.parse().unwrap_or(0);
+    }
+
+    println!("part2: {}", sum);
 
     Ok(())
 }
