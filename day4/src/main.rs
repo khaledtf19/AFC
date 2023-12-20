85a668c75cc5888fe3a854405bc18ae5212fa461
Khaledtf Labeb <khaledtf19@gmail.com>
2023-12-21 00:41:50 +0200

    day 4 part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,29 +1,61 @@
 use anyhow::Result;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::u32;
+
+#[derive(Debug, Clone)]
+struct Instances {
+    pub hm: HashMap<i32, i32>,
+}
+impl Instances {
+    fn new() -> Instances {
+        Instances { hm: HashMap::new() }
+    }
+    fn join(&mut self, key: i32, value: i32) {
+        if let Some(num) = self.hm.get(&key) {
+            self.hm.insert(key, num + value);
+        } else {
+            self.hm.insert(key, value + 1);
+        }
+    }
+    fn get_instances(&mut self, key: i32) -> i32 {
+        if let Some(instance) = self.hm.get(&key) {
+            instance.clone()
+        } else {
+            self.hm.insert(key, 1);
+            1
+        }
+    }
+}
 
 fn main() -> Result<()> {
     let file = File::open("input.txt")?;
     let reader = BufReader::new(file);
+    let mut instances = Instances::new();
     let mut result: Vec<u32> = Vec::new();
-    for line_maybe in reader.lines() {
+    for (y, line_maybe) in reader.lines().enumerate() {
         match line_maybe {
-            Ok(line) => part1(&mut result, line),
+            Ok(line) => resolve_line(&mut result, line, &mut instances, y as i32),
             Err(_) => {}
         }
     }
 
     let sum: u32 = result.iter().sum();
-    println!("{}", sum);
+
+    //  part 1 --->
+    println!("part 1: {}", sum);
+    //  part 2 --->
+    println!("part 2: {:?}", instances.hm.values().sum::<i32>());
     Ok(())
 }
-fn part1(result: &mut Vec<u32>, line: String) {
+fn resolve_line(result: &mut Vec<u32>, line: String, instances: &mut Instances, y: i32) {
     let mut start = false;
     let mut is_second = false;
     let mut arr_nums: Vec<u32> = Vec::new();
     let mut num_string = String::from("");
     let mut result_num: u32 = 0;
+    let ins = instances.get_instances(y + 1);
+    let mut count = 0;
 
     for (i, ch) in line.chars().enumerate() {
         if ch == ':' {
@@ -43,8 +75,10 @@ fn part1(result: &mut Vec<u32>, line: String) {
                 if arr_nums.contains(&num_string.parse().unwrap()) {
                     if result_num == 0 {
                         result_num = 1;
+                        count += 1;
                     } else {
-                        result_num *= 2
+                        result_num *= 2;
+                        count += 1;
                     }
                 }
                 num_string = String::from("");
@@ -58,13 +92,20 @@ fn part1(result: &mut Vec<u32>, line: String) {
                 if arr_nums.contains(&num_string.parse().unwrap_or(0)) {
                     if result_num == 0 {
                         result_num = 1;
+                        count += 1;
                     } else {
-                        result_num *= 2
+                        result_num *= 2;
+                        count += 1;
                     }
                 }
             }
             num_string = String::from("");
         }
     }
+
+    for i in y + 1..count + y + 1 {
+        instances.join(i + 1, ins);
+    }
+
     result.push(result_num);
 }
