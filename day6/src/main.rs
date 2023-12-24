76421214041221bc3456a26d6dd99c702d69725b
Khaledtf Labeb <khaledtf19@gmail.com>
2023-12-24 20:42:15 +0200

    day 6 part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -7,37 +7,58 @@ use std::{
 fn main() -> Result<(), Box<dyn Error>> {
     let file = File::open("input.txt")?;
     let reader = BufReader::new(file);
-    let mut times: Vec<u32> = Vec::new();
-    let mut dist: Vec<u32> = Vec::new();
-    let mut result: Vec<_> = Vec::new();
+    let mut times: Vec<String> = Vec::new();
+    let mut dists: Vec<String> = Vec::new();
+    let mut p1_result: Vec<_> = Vec::new();
+    let mut p2_result: u32 = 0;
 
     for line_maybe in reader.lines() {
         match line_maybe {
-            Ok(line) => resolve_line(line, &mut times, &mut dist),
+            Ok(line) => resolve_line(line, &mut times, &mut dists),
             Err(_) => {}
         }
     }
+    //  part 1 --->
     for (i, num) in times.iter().enumerate() {
-        let (time, dis) = (num, dist[i]);
+        let (time, dis) = (
+            num.parse::<u32>().unwrap(),
+            dists[i].parse::<u32>().unwrap(),
+        );
         let mut count = 0;
-        for speed in 1..*time {
+        for speed in 1..time {
             let d = speed * (time - speed);
             if d > dis {
                 count += 1
             }
         }
-        result.push(count)
+        p1_result.push(count)
     }
-    println!("{:?}", result.iter().fold(1, |state, num| state * num));
+    println!(
+        "part1: {:?}",
+        p1_result.iter().fold(1, |state, num| state * num)
+    );
+
+    //  part2 --->
+    let (time, dist): (u64, u64) = (
+        times.join("").parse().unwrap(),
+        dists.join("").parse().unwrap(),
+    );
+    for speed in 1..time {
+        let d = speed * (time - speed);
+        if d > dist {
+            p2_result += 1
+        }
+    }
+    println!("part2: {p2_result}");
     Ok(())
 }
-fn resolve_line(line: String, times: &mut Vec<u32>, dist: &mut Vec<u32>) {
+fn resolve_line(line: String, times: &mut Vec<String>, dist: &mut Vec<String>) {
     let split = line.split(": ").collect::<Vec<&str>>();
     let nums = split[1]
         .split(" ")
         .filter(|s| !s.is_empty())
-        .map(|s| s.parse().unwrap())
-        .collect::<Vec<u32>>();
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
     if split[0] == "Time" {
         *times = nums
     } else {
